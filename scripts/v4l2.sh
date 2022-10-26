43be4fe3e52528442e4e7929c2b19e6f6e042401
Cole Mickens <cole.mickens@gmail.com>
2022-10-26 06:40:53 -0700

    stash

--- scripts/v4l2.sh
+++ scripts/v4l2.sh
@@ -2,21 +2,25 @@
 
 set -x
 
-dname="$1"; shift
 videonr="$1"; shift
-
 v4dev="/dev/video${videonr}"
-address="ws://192.168.74.236:9443"
-address="ws://localhost:9443"
-# address="ws://0.0.0.0:9443"
+dname="${1-"${v4dev}"}"; shift
+
+# address="${pubaddress-"ws://localhost:9443"}"
+address="${pubaddress-"ws://localhost:9443"}"
+
 echo $GST_PLUGIN_SYSTEM_PATH
 export GST_PLUGIN_PATH="${PWD}/../target/release:${GST_PLUGIN_PATH}"
 gst-launch-1.0 \
   webrtcsink name="ws" signaller::address="${address}" meta="a-structure, display-name=${dname}" \
-  v4l2src device=${v4dev} name="src" ! \
-    videoconvert ! \
-    video/x-raw ! \
+  v4l2src device=${v4dev} name="src" do-timestamp=true ! \
+  image/jpeg,format=MJPG,width=1280,height=720,framerate=30/1 ! \
+  jpegparse ! jpegdec ! \
+  videoconvert ! \
   ws. \
+  # foo
+  
+    # videoconvert ! \
     # pulsesrc source-output-index=4 ! audioconvert ! audio/x-raw ! audioresample ! ws.
 
 sleep 5
